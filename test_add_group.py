04f6ef414d2d2529638e9d2f3e6383d498bc06ec
turnat <bipgoboj@gmail.com>
2020-06-24 14:17:09 +0300

    Рефакторинг вспомогательных классов

--- test_add_group.py
+++ test_add_group.py
@@ -1,47 +1,85 @@
 # -*- coding: utf-8 -*-
+import time
+# webdriver это и есть набор команд для управления браузером
 from selenium import webdriver
+import geckodriver_autoinstaller
+driver = webdriver.Chrome()
+import unittest, time, re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
-
-class TestAddGroup(unittest.TestCase):
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+#time.sleep(5)
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+#driver.get("http://localhost/addressbook/")
+#time.sleep(5)
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome()
+        #self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+       
+       
 
+
+    def logaut(self, wd):
+        wd.find_element_by_link_text("Выйти").click()
+        time.sleep(5)
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("Группы").click()
+
+    def create_group(self, wd, name, header, footer):
+        # init group creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        time.sleep(5)
+        # развернуть браузер на весь экран
+        wd.maximize_window()
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("Группы").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("NameG")
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("GLead")
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("bla bla bla")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="NameG", header="header", footer="bla bla bla")
+        self.return_to_groups_page(wd)
+        self.logaut(wd)
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logaut(wd)
 
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+
+
 
 
     def tearDown(self):
@@ -50,3 +88,59 @@ class TestAddGroup(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# После выполнения всех действий мы не должны забыть закрыть окно браузера
+#driver.quit()

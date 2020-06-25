b80317774943703e182d2b477f79ec601b86fae3
turnat <bipgoboj@gmail.com>
2020-06-25 10:25:09 +0300

    Создание нового файла для вспомагательных методов Applicftion
    В таком виде тест работает

--- application.py
+++ application.py
@@ -1,12 +1,7 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 driver = webdriver.Chrome()
 import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-
 
 class Application:
     def __init__(self):
